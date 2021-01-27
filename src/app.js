4f25b436c382a593a2d3e865b8b0ec994b84b183
aniledev <elinamcgill@gmail.com>
2021-01-27 10:01:42 -0500

    CHORE: create comment blocks

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const helmet = require("helmet");
 const cors = require("cors");
 const winston = require("winston");
 const { NODE_ENV, PORT } = require("./config");
-
 const app = express();
 
+// CONFIGURE LOGGING
 const morganOption = NODE_ENV === "production" ? "tiny" : "dev";
 
 const logger = winston.createLogger({
@@ -31,7 +31,18 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// API KEY HANDLING MIDDLE ON THE SERVER
+// this is where the validate bearer function goes
+
 //ROUTES
+// Write a route handler for the endpoint GET /bookmarks that returns a list of bookmarks
+
+// Write a route handler for the endpoint GET /bookmarks/:id that returns a single bookmark with the given ID, return 404 Not Found if the ID is not valid
+
+// Write a route handler for POST /bookmarks that accepts a JSON object representing a bookmark and adds it to the list of bookmarks after validation.
+
+// Write a route handler for the endpoint DELETE /bookmarks/:id that deletes the bookmark with the given ID.
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
